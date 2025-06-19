0fc053ccef7ddc20f7c518aae593068c6153b8e9
Andy Kitt <andy@openformat.tech>
2025-06-19 15:52:14 +0100

    Updated base-widget.tsx

--- components/base-widget.tsx
+++ components/base-widget.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { LucideIcon } from "lucide-react";
+import { DivideIcon as LucideIcon } from "lucide-react";
 import { ReactNode } from "react";
 
 interface BaseWidgetProps {
